81ebcb662dc5292702a50f8fc11f377547e2fd2b
Frank Breedijk <frank@breedijk.net>
2013-11-06 17:57:47 +0100

    FIrewall rules are found now...

--- recipes/manager.rb
+++ recipes/manager.rb
@@ -31,31 +31,47 @@ if ( not node["cloudstack"]["SECkey"] ) then
 end
 
 # Get rules from cloudstack
-#csapi = CloudstackRubyClient::Client.new(node['cloudstack']['url'],node['cloudstack']['APIkey'],node['cloudstack']['SECkey'])
 csapi = CloudStackHelper.new(:api_url => node['cloudstack']['url'],:api_key => node['cloudstack']['APIkey'],:secret_key => node['cloudstack']['SECkey'])
-#pfrules = csapi.list_network_offerings()
-#fwrules = csapi.list_firewall_rules()
 
 params = { 
   :command => "listPortForwardingRules",
   :response => 'json'
 }
 json =csapi.get(params).body
-pfrules = JSON.parse(json)
-Chef::Log.info(pfrules)
+pfrules = JSON.parse(json)["listportforwardingrulesresponse"]["portforwardingrule"]
 
 params = { 
   :command => "listFirewallRules",
   :response => 'json'
 }
 json =csapi.get(params).body
-fwrules = JSON.parse(json)
-Chef::Log.info(fwrules)
+fwrules = JSON.parse(json)["listfirewallrulesresponse"]["firewallrule"]
+
 # This should probably be a partial search, but I don't get the documentation 
 # for that feature
 nodes = search(:node, "cloudstack_firewall:*")
 
 nodes.each do |n|
   Chef::Log.info("Found firewall configuration for host: #{n.name}")
+  # Get all ingress rules
+  n["cloudstack"]["firewall"]["ingress"].each do |fw|
+    found = false
+    fwrules.each do |r|
+      if ( ( not found ) &&
+        fw[0] == r["ipaddress"] &&
+        fw[1] == r["protocol"] &&
+        fw[2] == r["cidrlist"] &&
+        fw[3] == r["startport"] &&
+        fw[4] == r["endport"] 
+      ) then
+        r["action"] = "keep"
+        found = true
+        Chef::Log.info("Match: #{r}")
+      else
+        Chef::Log.info("No match: #{fw[0]} - #{r["ipaddress"]} - #{r}")
+      end
+    end
+  end
 end
 
+Chef::Log.info(fwrules)
