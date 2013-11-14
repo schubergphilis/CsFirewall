098f609f69acf198305ac0d1fefce7760b59a01a
Frank Breedijk <frank@breedijk.net>
2013-11-14 16:39:32 +0100

    Making it Apache2.0 compliant prior to releasing it

--- recipes/prerequisites.rb
+++ recipes/prerequisites.rb
@@ -1,13 +1,23 @@
+# Author:: Frank Breedijk <fbreedijk@schubergphilis.com>
+# Copyright:: Copyright (c) 2013
+# License:: Apache License, Version 2.0
 #
-# Cookbook Name:: CsFirewall
-# Recipe:: manager
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Copyright 2013, Schuberg Philis
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# All rights reserved - Do Not Redistribute
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This recipe is run by those nodes that manage the couldstack firewall
+# Cookbook Name:: CsFirewall
+# Recipe:: prerequisites
+
+# This recipe installs the prerequisite gem for the manager recipe
 
 Chef::Log.info("Start of CsFirewall::prerequisites recipe")
 
